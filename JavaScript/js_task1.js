9e52eed5575b485b7eb60ca0dca5b7c383de596d
moh-x <mo.xodeeq@gmail.com>
2020-03-26 22:22:14 +0100

    update upto 26 Mar 2020

--- JavaScript/js_task1.js
+++ JavaScript/js_task1.js
@@ -1,16 +1,24 @@
-let name = "Babatunde";
+let name = "Babatunde Adenowo";
 const courses = ['html-css-js', 'php', 'node.js', 'java', 'flutter', 'kotlin', 'go',];
+
+// Printing out my name and courses
+console.log(name);
+console.log("Courses:");
+for (index=0; index < courses.length; index++) { 
+    console.log(courses[index]);
+} 
+
 const data = []
 
-if (courses.length%2!==0){
-    for (let i=0; i<=200; i++){
-        if (i%2!==0){
+if (courses.length%2!==0) {
+    for (let i=0; i<=200; i++) {
+        if (i%2!==0) {
             data.push(i);
         }
     }
 } else {
-    for (let i=0; i<=200; i++){
-        if (i%2==0){
+    for (let i=0; i<=200; i++) {
+        if (i%2==0) {
             data.push(i);
         }
     }

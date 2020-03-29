b06966c405b3b70b2eb6f55755c1eb0d27f05d3c
moh-x <mo.xodeeq@gmail.com>
2020-03-29 12:50:02 +0100

    update upto 29 Mar 2020

---
+++ Python/circle.py
@@ -0,0 +1,8 @@
+# Calculates the area of a Circle from a given Radius.
+
+r = int(input("Please input the circle radius and hit Enter "))
+pi = 3.14
+
+A = pi * r**2
+
+print("Area of the circle = "+str(A))
